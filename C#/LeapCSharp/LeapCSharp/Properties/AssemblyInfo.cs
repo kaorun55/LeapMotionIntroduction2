e00f4354b0d7e3d8bf43aff577ff15e56d8977b3
Kaoru NAKAMURA <kaorun55@gmail.com>
2015-08-09 23:24:36 +0900

    サンプルプロジェクトを差し替え

--- C#/LeapCSharp/LeapCSharp/Properties/AssemblyInfo.cs
+++ C#/LeapCSharp/LeapCSharp/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // アセンブリに関する一般情報は以下の属性セットをとおして制御されます。
 // アセンブリに関連付けられている情報を変更するには、
 // これらの属性値を変更してください。
-[assembly: AssemblyTitle( "LeapSample01" )]
+[assembly: AssemblyTitle( "LeapCSharp" )]
 [assembly: AssemblyDescription( "" )]
 [assembly: AssemblyConfiguration( "" )]
 [assembly: AssemblyCompany( "" )]
-[assembly: AssemblyProduct( "LeapSample01" )]
+[assembly: AssemblyProduct( "LeapCSharp" )]
 [assembly: AssemblyCopyright( "Copyright ©  2015" )]
 [assembly: AssemblyTrademark( "" )]
 [assembly: AssemblyCulture( "" )]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible( false )]
 
 // 次の GUID は、このプロジェクトが COM に公開される場合の、typelib の ID です
-[assembly: Guid( "c9def153-9585-4614-a868-c2172a234c5f" )]
+[assembly: Guid( "62afd120-68b6-4c8e-a14d-5e1a50f0f9b2" )]
 
 // アセンブリのバージョン情報は、以下の 4 つの値で構成されています:
 //
